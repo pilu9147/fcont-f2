9f83c6e9985572f3fc0197cb9f00dac4bf4d274e
pilu9147 <priyankabhesaniya617@gmail.com>
2023-05-13 17:18:47 +0530

    updated

--- script.js
+++ script.js
@@ -8,20 +8,56 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+  arr.map((stud)=>{
+    if(stud.marks > 50){
+      console.log(stud);
+    }
+  })
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+   
+  arr.forEach((stud)=>{
+    if(stud.marks > 50){
+      console.log(stud);
+    }
+  })
+    
 }
 
 function addData() {
   //Write your code here, just console.log
+  let stud = {
+    id:4,
+    name:"susan",
+    age:"20",
+    marks:45
+  }
+  arr.push(stud);
+  console.log(arr);
 }
+
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+  let res = arr.filter((student)=>{
+    if(student.marks < 50){
+      return false;
+    }else{
+      return true;
+    }
+  })
+  console.log(res);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let arr1 = [
+    { id: 5, name: "wickjhon", age: "22", marks: 45 },
+    { id: 6, name: "jacksparrow", age: "25", marks: 95 },
+    { id: 7, name: "pilu", age: "26", marks: 75},
+  ];
+  let ans = arr.concat(arr1);
+  console.log(ans)
 }
